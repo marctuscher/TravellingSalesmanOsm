f7bb7d40bbe4eae8f3a9283eb6addfc7f571625e
Marc Tuscher <marc.tuscher@gmail.com>
2018-09-21 16:10:46 +0200

    successfull eject

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../css/index.css';
-import App from './App';
-import registerServiceWorker from '../registerServiceWorker';
+import './css/index.css';
+import App from './js/App';
+import registerServiceWorker from './js/registerServiceWorker';
 
 import {Provider} from 'react-redux'
 
-import store from './bootstrap'
+import store from './js//bootstrap'
 
 
 ReactDOM.render(
--- react-client/src/js/App.js
+++ react-client/src/js/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/logo.svg';
+import '../css/App.css';
 import Sidebar from "react-sidebar";
 import MapView from './components/MapView'
 
